2f4890684bda618c8186e23d2ccde9c66bf45341
Dax Ledesma <37383659+dakilaledesma@users.noreply.github.com>
2022-12-13 13:15:41 -0500

    All joint visualization by joint-indices dictionary

--- SMPL_Visualizer/Assets/Scripts/SMPL_Controller.cs
+++ SMPL_Visualizer/Assets/Scripts/SMPL_Controller.cs
@@ -7,10 +7,37 @@ public class SMPL_Controller : MonoBehaviour
 {
     Vector3[,] joint_motions;
     int frame_counter = 0;
+    // int angle = 90;
+
+    Dictionary<string, int> joint_indices = new Dictionary<string, int>
+    {
+        ["hip"] = 0,
+        ["upperleg_l"] = 1,
+        ["upperleg_r"] = 2,
+        ["spine_01"] = 3,
+        ["lowerleg_l"] = 4,
+        ["lowerleg_r"] = 5,
+        ["spine_02"] = 6,
+        ["foot_l"] = 7,
+        ["foot_r"] = 8,
+        ["spine_03"] = 9,
+        ["ball_l"] = 10,
+        ["ball_r"] = 11,
+        ["neck"] = 12,
+        ["shoulder_l"] = 13,
+        ["shoulder_r"] = 14,
+        ["head"] = 15,
+        ["upperarm_l"] = 16,
+        ["upperarm_r"] = 17,
+        ["lowerarm_l"] = 18,
+        ["lowerarm_r"] = 19,
+        ["hand_l"] = 20,
+        ["hand_r"] = 21
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("a");
         string fileData = System.IO.File.ReadAllText("Assets/Data/out.csv");
         string[] lines = fileData.Split("\n");
         joint_motions = new Vector3[lines.Length, lines[0].Split(",").Length / 3];
@@ -29,8 +56,8 @@ public class SMPL_Controller : MonoBehaviour
     void Update()
     {
         foreach (Transform t in gameObject.GetComponentsInChildren<Transform>()){
-            if (t.name == "lowerleg_r"){
-                t.rotation = Quaternion.AngleAxis(270, joint_motions[frame_counter, 5]);
+            if (joint_indices.ContainsKey(t.name)){
+                t.rotation = Quaternion.Euler(joint_motions[frame_counter, joint_indices[t.name]]);
             }
         }
 
@@ -38,6 +65,7 @@ public class SMPL_Controller : MonoBehaviour
             frame_counter += 1;
         } else {
             frame_counter = 0;
+            // angle += 45;
         }
     }
 }
